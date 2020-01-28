59d2031d893230fad16e730fccd62f3c05848618
Em Emm M <emm@Ems-MacBook-Pro.local>
2020-01-28 14:35:19 -0800

    created game logic

---
+++ memory-game/JS/main.js
@@ -0,0 +1,6 @@
+let cardOne = "queen";
+let cardTwo = "king";
+let cardThree = "queen";
+let cardFour = "king";
+console.log ("User Flipped" + " " + cardOne);
+console.log ("User Flipped" + " " + cardThree);
