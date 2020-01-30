a10888d3ab4a30197e25ede42d72d2ec459ba7b9
Em Emm M <emm@Ems-MacBook-Pro.local>
2020-01-29 16:04:03 -0800

    created game logic

--- memory-game/JS/main.js
+++ memory-game/JS/main.js
@@ -1,6 +1,21 @@
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
-console.log ("User Flipped" + " " + cardOne);
-console.log ("User Flipped" + " " + cardThree);
+const cards = ['queen', 'queen', 'king', 'king'];
+const cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log ('user flipped queen');
+
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log (cardsInPlay);
+console.log ('User flipped queen');
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[2])
+	{
+		alert("You found a match!");
+	} 
+	else
+	{
+		alert("Sorry, try again!");
+	}
+}
