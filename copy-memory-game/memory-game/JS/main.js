2d746e8411defa06449eeb684652a67691354bb5
Em Emm M <emm@Ems-MacBook-Pro.local>
2020-02-01 18:54:02 -0800

    updated game logic

--- copy-memory-game/memory-game/JS/main.js
+++ copy-memory-game/memory-game/JS/main.js
@@ -1,21 +1,50 @@
-const cards = ['queen', 'queen', 'king', 'king'];
+const cards = [
+	{
+		rank: "Queen",
+		suit: "Hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "Queen",
+		suit: "Diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "King",
+		suit: "Hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "King",
+		suit: "Diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 const cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log ('user flipped queen');
 
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log (cardsInPlay);
-console.log ('User flipped queen');
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[2])
+function checkForMatch ()
+{	
+if (cardsInPlay.length === 2) 
 	{
-		alert("You found a match!");
-	} 
-	else
-	{
-		alert("Sorry, try again!");
+		if (cardsInPlay[0] === cardsInPlay[1])
+		{
+			console.log ("You found a match!");
+		} 
+		else
+		{
+			console.log ("Sorry, try again!");
+		}
 	}
 }
+
+function flipCard (cardID) 
+{
+console.log ("user flipped " + cards[cardID].rank) //identifies card user clicks
+cardsInPlay.push (cards[cardID].rank)
+console.log (cards[cardID].suit) //log the suit of the card to console
+console.log (cards[cardID].cardImage) //log the image of the card to console
+checkForMatch ()
+}
+flipCard (0)
+flipCard (2)
+
